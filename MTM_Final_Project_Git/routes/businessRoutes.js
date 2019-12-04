cca924668b7dd4a8dfbb5a4b0d2c35bdaab2a6e2
Daylan <gobstys@gmail.com>
2019-12-04 15:03:18 -0700

    Update
    ide instead of items and added voting system.

--- MTM_Final_Project_Git/routes/businessRoutes.js
+++ MTM_Final_Project_Git/routes/businessRoutes.js
@@ -4,32 +4,32 @@ require("../database")();
 
 const router = express.Router();
 
-router.route("/Items").get(function(req, res) {
-    getItems().then(function(items) {
+router.route("/Ides").get(function(req, res) {
+    getIdes().then(function(ides) {
         // convertJSONToMongo();
         var model = {
-            items,
+            ides,
             username: req.session.username,
             isAdmin: req.session.isAdmin,
-            css2link: "/css/items.css",
-            title: "Shop for Items",
-            desc: "All Items"
+            css2link: "/css/ides.css",
+            title: "Shop for Ides",
+            desc: "All Ides"
         };
-        res.render("items", model);
+        res.render("Ides", model);
     });
 });
 
 router.route("/Order").get(function(req, res) {
     if(req.session.username) {
-        let itemID = req.body.itemID;
-        if(!itemID) itemID = "";
+        let ideID = req.body.ideID;
+        if(!ideID) ideID = "";
         var model = {
-            itemID,
+            ideID,
             title: "Order",
             css2link: "/css/order.css",
             username: req.session.username,
             isAdmin: req.session.isAdmin
-        };
+        }
     
         res.render("order", model);
     } else {
@@ -38,9 +38,9 @@ router.route("/Order").get(function(req, res) {
 });
 
 router.route("/Order/:orderID").get(function(req, res) {
-    let itemID = req.params.orderID;
+    let ideID = req.params.orderID;
 
-    req.body.itemID = itemID;
+    req.body.ideID = ideID;
     req.url = "/Order";
     router.handle(req, res);
 });
@@ -55,67 +55,138 @@ router.route("/Order").post(function(req, res) {
     res.render("order_complete", model);
 });
 
-router.route("/Items/details/:itemID").get(function(req, res) {
-    let itemID = req.params.itemID;
+router.route("/Ides/details/:ideID").get(function(req, res) {
+    let ideID = req.params.ideID;
     
-    getItemByID(itemID).then(function(item) {
+    getIdeByID(ideID).then(function(ide) {
         let model = {
-            css2link: "/css/items.css",
-            title: "Shop for Items",
-            item,
+            css2link: "/css/ides.css",
+            title: "Shop for Ides",
+            ide,
             username: req.session.username,
             isAdmin: req.session.isAdmin,
-            desc: item.name
+            desc: ide.name
         };
-        res.render("item_details", model);
+        res.render("ide_details", model);
     });
 });
 
-router.route("/NewItem").get(function(req, res) {
+router.route("/NewIde").get(function(req, res) {
     if(req.session.isAdmin) {
         var model = {
-            title: "New Item",
+            title: "New Ide",
             css2link: "/css/order.css",
             username: req.session.username,
             isAdmin: req.session.isAdmin
         };
-        res.render("new_item", model);
+        res.render("new_ide", model);
     } else {
         res.redirect("/Login");
     }
 });
 
-router.route("/NewItem").post(function(req, res) {
+router.route("/NewIde").post(function(req, res) {
     if(req.session.isAdmin) {
-        createItem(req.body.category, req.body.name, req.body.imgPath, req.body.price, req.body.desc)
-            .then(function(item) {
-                res.redirect("/Items/details/" + item._id);
+        createIde(req.body.category, req.body.name, req.body.imgPath, req.body.price, req.body.desc)
+            .then(function(ide) {
+                res.redirect("/Ides/details/" + ide._id);
             });
     } else {
         res.sendStatus(401);
     }
 });
+//
+router.route("/Admin").get(async function (req, res) {
+    if(!req.session.isAdmin){
+        res.redirect("/");
+    }else {
+        var UsersFromDB = await User.find();
 
-// router.route("/user").get(
-//     async function (req, res) {
-//         if(!req.session.username) {
-//             res.redirect("/");
-//         }else {
-//             var userId = req.session.userId;
-//             var user = await User.findOne({_id:userId});
-//             console.log(user)
-//             if (user) {
-//                 var model = {
-//                     title: "User Profile",
-//                     user: user
-//                 }
-//                 res.render("user", model);
+        var model = {
+            css2link: "/css/profile.css",
+            title: "Admin CP",
+            users: UsersFromDB,
+            username : req.session.username,
+            userId : req.session.userId,
+            isAdmin : req.session.isAdmin,
+            ide: req.session.ide
+        };
+        res.render("admin", model);
+    }
+});
 
-//             } else {
-//                 res.send("You done messed up! Could not find a user with id: " + userId);
-//             }
-//         }
-//     }
-// );
+router.route("/ToAdmin/:userId").get(async function (req, res) {
+    if(!req.session.isAdmin) {
+        res.redirect("/");
+    } else {
+        var userId = req.params.userId;
+        var user = await User.findOne({_id: userId});
+        if(!user.roles.includes("Admin")) {
+            user.roles.push("Admin");
+            User.findByIdAndUpdate(userId, Object(user), {useFindAndModify: false}, function(err, doc) {
+                if(err) console.log("There is an error.");
+                console.log(doc);
+            });
+        }
+        res.redirect("/Admin");
+    }
+});
+
+router.route("/user").get(
+    async function (req, res) {
+        if(!req.session.username) {
+            res.redirect("/");
+        }else {
+            var userId = req.session.userId;
+            var user = await User.findOne({_id:userId});
+            console.log(user)
+            if (user) {
+                var model = {
+                    title: "User Profile",
+                    user: user,
+                    username: req.session.username,
+                    userId: req.session.userId,
+                    isAdmin: req.session.isAdmin,
+                    ide: req.session.ide
+                }
+                res.render("profile", model);
+
+            } else {
+                res.send("You done messed up! Could not find a user with id: " + userId);
+            }
+        }
+    }
+);
+
+
+router.route("/submitForm").post(
+    async function (req, res) {
+        if(!req.session.username) {
+            res.redirect("/");
+        }else {
+            var userId = req.session.userId;
+            var user = await User.findOne({_id:userId});
+            var result = req.body.resp;
+            req.session.ide = result;
+            user.ide = result;
+            User.findByIdAndUpdate(userId, Object(user), {useFindAndModify: false}, function(err, doc) {
+                if(err) console.log("There is an error.");
+                console.log(doc);
+            });
+            if (user) {
+                var model = {
+                    title: "User Profile",
+                    user: user,
+                    username: req.session.username,
+                    userId: req.session.userId,
+                    isAdmin: req.session.isAdmin,
+                    ide: req.body.resp
+                }
+                res.render("profile", model);
+            }
+        }
+    }
+)
+
 
 module.exports = router;
